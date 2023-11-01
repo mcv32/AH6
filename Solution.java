09f0b53bb20fc27e52f706365e9006c366dddcf9
mcv32 <marcus.villatoro@fiserv.com>
2023-11-01 15:28:09 -0400

    first commit with solution

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -15,10 +16,15 @@ class Node<T> {
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
     // todo
-    return;
+    List<String> list = new ArrayList<>();
+    while(head != null){
+      list.add(head.val);
+      head = head.next;
+    }
+    return list;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -29,7 +35,7 @@ class Solution {
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    System.out.println(Solution.linkedListValues(a));
     // -> [ "a", "b", "c", "d" ]
   }
 }
